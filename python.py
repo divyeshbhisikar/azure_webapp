5254cc4b3b3c5893f688efde8edbfc8b8ee633db
divyeshbhisikar <97149958+divyeshbhisikar@users.noreply.github.com>
2022-03-26 18:03:18 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+pip install snowflake-connector-python[pandas]
+import snowflake.connector
+import pandas as pd
+import numpy as np
+ctx = snowflake.connector.connect(
+    account = 'pd91876.ap-south-1',
+    user = 'Demo',
+    schema = 'Public',
+    warehouse='Compute_wh',
+    role = 'accountadmin',
+    password = 'Qwerty@1234'
+)
+cur = ctx.cursor()
+query = '''select * from "test"."public"."B"'''
+data = pd.read_sql(query, ctx)
+
+display(data)
