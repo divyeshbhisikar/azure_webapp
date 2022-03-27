58670820d64213c4a47fc0e23edcfbca24cf2fda
divyeshbhisikar <97149958+divyeshbhisikar@users.noreply.github.com>
2022-03-28 00:58:56 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import snowflake.connector
+import pandas as pd
+import numpy as np
+ctx = snowflake.connector.connect(
+    account = 'pd91876.ap-south-1',
+    user = 'Demo',
+    schema = 'Public',
+    warehouse='Compute_wh',
+    role = 'accountadmin',
+    password = 'Qwerty@1234'
+)
+cur = ctx.cursor()
+query = '''select * from "test"."public"."B"'''
+data = pd.read_sql(query, ctx)
+print(data)
